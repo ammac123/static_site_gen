2b433ad704d7a36414554da04ee1245d4ed77d15
Andrew Macleod <andrew.munro.macleod@gmail.com>
2026-04-09 21:39:42 +1000

    Updated gitpage links

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import sys
 from copystatic import copy_directory_src_to_dest
 from generatepage import generate_page_recursive
 
-basepath = sys.argv[1] or "/"
+basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
 dir_path_static = "static"
 dir_path_public = "docs"
 dir_path_content = "content"
