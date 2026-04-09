b84b3fe03b67442c2304bd52d21aa4b309d01899
Andrew Macleod <andrew.munro.macleod@gmail.com>
2026-04-09 21:10:43 +1000

    Added docs for GitLab page

--- src/generatepage.py
+++ src/generatepage.py
@@ -2,7 +2,7 @@ import os
 import shutil
 from markdown_blocks import markdown_to_html_node, extract_title
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f'Generating page from "{from_path}" to "{dest_path}" using "{template_path}')
     with open(from_path, 'r') as file:
         markdown = file.read()
@@ -10,21 +10,24 @@ def generate_page(from_path, template_path, dest_path):
         template = file.read()
     content = markdown_to_html_node(markdown).to_html()
     title = extract_title(markdown)
-    html_page = template.replace("{{ Title }}", title).replace("{{ Content }}", content)
+    html_page = template.replace("{{ Title }}", title)
+    html_page = html_page.replace("{{ Content }}", content)
+    html_page = html_page.replace('href="/', f'href="{basepath}')
+    html_page = html_page.replace('src="/', f'src="{basepath}')
 
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w") as file:
         file.write(html_page)
 
-def generate_page_recursive(dir_path_content, template_path, dir_dest_path):
+def generate_page_recursive(dir_path_content, template_path, dir_dest_path, basepath):
     os.makedirs(dir_dest_path, exist_ok=True)
     for item in os.listdir(dir_path_content):
         content_path: str = os.path.join(dir_path_content, item)
         dest_path: str = os.path.join(dir_dest_path, item)
         if os.path.isdir(content_path):
-            generate_page_recursive(content_path, template_path, dest_path)
+            generate_page_recursive(content_path, template_path, dest_path, basepath)
             continue
         if os.path.isfile(content_path) and content_path.endswith(".md"):
             dest_fp = dest_path.removesuffix(".md") + '.html'
-            generate_page(content_path, template_path, dest_fp) # type: ignore
+            generate_page(content_path, template_path, dest_fp, basepath) # type: ignore
     return
--- src/main.py
+++ src/main.py
@@ -1,18 +1,21 @@
 import os
 import shutil
+import sys
 from copystatic import copy_directory_src_to_dest
 from generatepage import generate_page_recursive
 
+basepath = sys.argv[0] or "/"
 dir_path_static = "static"
-dir_path_public = "public"
+dir_path_public = "docs"
 dir_path_content = "content"
 template_path = "template.html"
 
 def main():
+    print(basepath)
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
     copy_directory_src_to_dest(".", dir_path_static, dir_path_public)
-    generate_page_recursive(dir_path_content, template_path, dir_path_public)
+    generate_page_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 if __name__=="__main__":
     main()
